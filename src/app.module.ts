dcfd7d6d5f0ff04d197f135320e1696a5d6c34a1
Pierre Oliveira <pierre.torres445@gmail.com>
2023-09-15 01:14:02 -0300

    feat: implement modules and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidation } from './utils/zod-validation.utils';
+import { PrismaService } from './prisma/prisma.service';
+import { S3Service } from './s3/s3.service';
+import { ConfigModule } from '@nestjs/config';
+import { CategoriesModule } from './categories/categories.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CategoriesModule,
+    MenuModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidation,
+    },
+    PrismaService,
+    S3Service,
+  ],
 })
 export class AppModule {}
